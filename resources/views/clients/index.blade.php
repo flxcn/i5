1b4690f0872ec29d5189936fe9d23111e1d8e41c
Felix <30987125+flxcn@users.noreply.github.com>
2022-05-30 22:46:44 -0500

    Create index.blade.php

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="bg-white p-6 rounded">
+            Posts
+        </div>
+    </div>
+@endsection

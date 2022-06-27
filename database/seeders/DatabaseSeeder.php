faf6b398d8b82b0c952ae788c49949a4f3ec4965
Felix <30987125+flxcn@users.noreply.github.com>
2022-06-26 22:17:23 -0400

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Client;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Client::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

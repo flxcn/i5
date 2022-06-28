e8c7cd5800859d637102115201ae1d9f87029b36
Felix <30987125+flxcn@users.noreply.github.com>
2022-06-28 18:31:43 -0400

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('SCAS Client Contact Database') }}</div>
+            <div class="card mb-3">
+                <div class="card-header">{{ __('SCAS i5 Client Contact Database') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <p>Welcome to the Small Claims Advisory Service's <b>i5 database</b>!<br>You can find the code repository for this platform <a href="https://github.com/flxcn/i5">here</a>.</p> 
+                </div>
+            </div>
 
-                    {{ __('Welcome to the Small Claims Advisory Service\'s i5 database. For any questions, please contact felixchen@college.harvard.edu') }}
+            <div class="card">
+                <div class="card-header text-success">Updates from the i4</div>
+
+                <div class="card-body">
+                    <p>The i5 is a completely rewritten-from-scratch database platform for tracking our client contacts.<br>A few important differences:</p>
+                    <ul>
+                        <li>Your username is now your @college email.</li>
+                        <li>For previous i4 users, please click "Forgot Your Password?" in order to set a new one.</li>
+                    </ul>
+                    <p>For any questions, please contact <a href="mailto:{{ __('messages.webmaster_email')}}">{{ __('messages.webmaster_name')}}</a>.</p>
                 </div>
             </div>
         </div>

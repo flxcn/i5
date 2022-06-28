6bf22785ae70ee798ac50078ab23ca4f512d7596
Felix <30987125+flxcn@users.noreply.github.com>
2022-06-28 03:25:34 -0400

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,13 +7,22 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $this->middleware('auth');
     }
 
     /**

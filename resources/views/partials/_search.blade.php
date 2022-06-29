7007e84e25d2cc4f1f662e1a52c0cd746d47028c
Felix <30987125+flxcn@users.noreply.github.com>
2022-06-28 20:25:42 -0400

    Update _search.blade.php

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,8 +1,9 @@
 <div class="d-flex ms-5">  
-    <a href="/clients/create" class="btn btn-outline-success">Add&nbsp;Client</a>
     <form action="/search" method="get">  
         <div class="input-group ms-1">
-            <input type="text" name="q" class="form-control bg-white" placeholder="Search the i5..." aria-label="Search the i5..." value= @if(isset($clients)) {{ request()->query('q'); }} @else {{ "" }} @endif>
+            <a href="/clients/create" class="btn btn-outline-success rounded">Add&nbsp;Client</a>
+
+            <input type="text" name="q" class="form-control bg-white ms-2 rounded-start" placeholder="Search the i5..." aria-label="Search the i5..." value= @if(isset($clients)) {{ request()->query('q'); }} @else {{ "" }} @endif>
             <button class="btn btn-outline-primary" type="submit">Find&nbsp;Client</button>
             <button type="button" class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
@@ -13,7 +14,7 @@
                 <li><button class="dropdown-item" type="submit" name="mode" value="phone">By Phone</button></li>
 
                 <li><hr class="dropdown-divider"></li>
-                
+
                 <li><a class="dropdown-item" href="/clients">Most Recent</a></li>
                 <li><a class="dropdown-item" href="#">Most Urgent</a></li>
             </ul>

5aebda407eeac46194398a44a4354d49eec2bea3
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-02 23:25:39 -0400

    Update Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -11,6 +11,8 @@ class Client extends Model
     use HasFactory;
     use Searchable;
 
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone_number'];
+
     /**
      * Get the name of the index associated with the model.
      *

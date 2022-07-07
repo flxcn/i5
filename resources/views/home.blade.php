2a46fdbdc62964d7d3efa997e0afb6239dd64954
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-07 02:52:56 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card mb-3">
+            {{-- <div class="card mb-3">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body text-success">
@@ -16,7 +16,7 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-            </div>
+            </div> --}}
 
             <div class="card mb-3">
                 <div class="row">
@@ -38,9 +38,9 @@
                     <div class="card-body">
                         <h5 class="card-title">My Contributions</h5>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Clients entered: <b>9</b></li>
-                            <li class="list-group-item">Contacts recorded: <b>12</b></li>
-                            <li class="list-group-item">Contacts edited: <b>3</b></li>
+                            <li class="list-group-item">Clients entered: <b>9</b> (<a href="/clients/author_id={{Auth::user()->id}}">See all</a>)</li>
+                            <li class="list-group-item">Contacts recorded: <b>12</b> (<a href="/clients/author_id={{Auth::user()->id}}">See all</a>)</li>
+                            {{-- <li class="list-group-item">Contacts edited: <b>3</b></li> --}}
                         </ul>
                     </div>
                 </div>
@@ -54,6 +54,80 @@
 
                     contacts/index
                 </div> --}}
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-header">{{ __('My clients') }}</div>
+
+                <div class="card-body p-4">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Client ID</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Phone #</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Language</th>
+                                <th scope="col">State</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {{-- @foreach($clients as $client)
+                                    <tr>
+                                        <th scope="row">{{$client->id}}</th>
+                                        <td>{{$client->first_name}}</td>
+                                        <td>{{$client->last_name}}</td>
+                                        <td>{{$client->phone_number}}</td>
+                                        <td>{{$client->email}}</td>
+                                        <td>{{$client->language}}</td>
+                                        <td>{{$client->state}}</td>
+                                    </tr>
+                                @endforeach  --}}
+                            </tbody>
+                        </table>
+                    </div>
+                {{-- {{$clients->links()}} --}}
+                </div>
+                
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-header">{{ __('Recent contacts') }}</div>
+
+                <div class="card-body p-4">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Client ID</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Phone #</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Language</th>
+                                <th scope="col">State</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {{-- @foreach($clients as $client)
+                                    <tr>
+                                        <th scope="row">{{$client->id}}</th>
+                                        <td>{{$client->first_name}}</td>
+                                        <td>{{$client->last_name}}</td>
+                                        <td>{{$client->phone_number}}</td>
+                                        <td>{{$client->email}}</td>
+                                        <td>{{$client->language}}</td>
+                                        <td>{{$client->state}}</td>
+                                    </tr>
+                                @endforeach  --}}
+                            </tbody>
+                        </table>
+                    </div>
+                {{-- {{$clients->links()}} --}}
+                </div>
+                
             </div>
         </div>
     </div>

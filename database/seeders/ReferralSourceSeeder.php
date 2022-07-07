e70d7a1b8ef60db80490b1a570a6cbffa07f6a5b
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-07 03:01:59 -0400

    Create ReferralSourceSeeder.php

---
+++ database/seeders/ReferralSourceSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\ReferralSource;
+
+class ReferralSourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ReferralSource::truncate();
+
+        $referral_sources =   [
+            [
+                'id' => 1,
+                'description' => 'None Selected',
+            ],
+            [
+                'id' => 2,
+                'description' => 'Other',
+            ],
+            [
+                'id' => 3,
+                'description' => 'Flier/Brochure',
+            ],
+            [
+                'id' => 4,
+                'description' => 'Friend/Family',
+            ],
+            [
+                'id' => 5,
+                'description' => 'Attorney General',
+            ],
+            [
+                'id' => 6,
+                'description' => 'Greater Boston Legal Services',
+            ],
+            [
+                'id' => 7,
+                'description' => 'Search Engine',
+            ],
+            [
+                'id' => 8,
+                'description' => 'MBTA Ad',
+            ],
+        ];
+
+        ReferralSource::insert($referral_sources);
+    }
+}

72963504e44565792bd2046a9aaa02e5a8ef2d47
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-07 02:54:10 -0400

    Create CategorySeeder.php

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::truncate();
+
+        $categories =   [
+            [
+                'id' => 1,
+                'description' => 'None Selected',
+                'is_active' => True,
+            ],
+            [
+                'id' => 2,
+                'description' => 'Not Small Claims',
+            ],
+            [
+                'id' => 3,
+                'description' => 'General Procedure',
+            ],
+            [
+                'id' => 4,
+                'description' => 'Contracted Services',
+            ],
+            [
+                'id' => 5,
+                'description' => 'Merchandise and Goods',
+            ],
+            [
+                'id' => 6,
+                'description' => 'Personal Disputes',
+            ],
+            [
+                'id' => 7,
+                'description' => 'Collection from Judgements',
+            ],
+            [
+                'id' => 8,
+                'description' => 'Credit/Debit Disputes',
+            ],
+            [
+                'id' => 9,
+                'description' => 'Landlord/Tenant Law',
+            ],
+            [
+                'id' => 10,
+                'description' => 'Auto Law',
+            ],
+            [
+                'id' => 11,
+                'description' => 'Wage Law',
+            ],
+        ];
+
+        Category::insert($categories);
+    }
+}

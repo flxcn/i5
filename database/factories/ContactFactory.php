fb1be00321dc63266d1075ecc810a127121ad89f
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-10 14:30:05 -0400

    Create ContactFactory.php

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Client;
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $clients = Client::all()->pluck('id')->toArray();
+
+        $users = User::all()->pluck('id')->toArray();
+
+        return [
+            'client_id' => $this->faker->randomElement($clients),
+            'originator_id' => $this->faker->randomElement($users),
+            'editor_id' => 1,	
+            'contact_type_id' => 99,	
+            'contact_date' => $this->faker->date(),
+            'contact_summary' => $this->faker->text(1000),
+            // created_at	
+            // updated_at
+        ];
+    }
+}

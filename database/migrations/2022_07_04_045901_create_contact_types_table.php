f94dbeb9bdad000b6b8f7e82e69ea4a923b69225
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-10 01:48:29 -0400

    Update 2022_07_04_045901_create_contact_types_table.php

--- database/migrations/2022_07_04_045901_create_contact_types_table.php
+++ database/migrations/2022_07_04_045901_create_contact_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('contact_types', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->boolean('is_active');
             $table->timestamps();
         });
     }

2ac367cb950ee3f6a35a0ef3826b7a58e42907f3
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-11 14:50:24 -0400

    Create index.blade.php

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+
+            @if(session()->has('message'))
+            <div class="card mb-3 bg-success">
+                <div class="card-body p-3">
+                <p class="card-text text-white">
+                    {{session('message')}}
+                    <a class="link-light" href="/clients/{{session('client_id')}}/contacts/{{session('contact_id')}}">Click here</a> to view.
+                </p>
+                </div>
+            </div>
+            @endif
+
+            @unless(count($contacts) == 0)
+            
+            <div class="card mb-3">
+                <div class="card-body p-4">
+                    <h2 class="card-title mb-2">Contacts with {{$client->first_name}} {{$client->last_name}}</h2>
+                    <h6 class="card-subtitle mb-3 text-muted">(<a href="{{route('clients.show',$client->id)}}">Back to client</a>)</h6>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Contact ID</th>
+                                    <th scope="col">Contact Type</th>
+                                    <th scope="col">Contact Date</th>
+                                    <th scope="col">Summary</th>
+                                    <th scope="col">Created At</th>
+                                    <th scope="col">Updated At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($contacts as $contact)
+                                    <tr>
+                                        <th scope="row">{{$contact->id}}</th>
+                                        <td>{{$contact->contact_type_id}}</td>
+                                        <td>{{$contact->contact_date}}</td>
+                                        <td>{{Str::limit($contact->contact_summary, 1000) }}</td>
+                                        <td>{{$contact->created_at}}</td>
+                                        <td>{{$client->updated_at}}</td>
+                                    </tr>
+                                @endforeach 
+                            </tbody>
+                        </table>
+                    </div>
+                {{$contacts->links()}}
+                </div>
+            </div>
+            @else
+                <p>No contacts found</p>
+            @endunless
+        </div>
+    </div>
+</div>    
+@endsection

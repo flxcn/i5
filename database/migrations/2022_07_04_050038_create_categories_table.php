a15ed90761927a0e99a76c1d9072571a2db896c3
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-14 22:57:35 -0400

    Update 2022_07_04_050038_create_categories_table.php

--- database/migrations/2022_07_04_050038_create_categories_table.php
+++ database/migrations/2022_07_04_050038_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unsigned();
             $table->string('description');
             $table->timestamps();
         });

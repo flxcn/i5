21e6224db0e791075a2fe87b6ffbabedf11dc1b9
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-14 22:57:40 -0400

    Update 2022_07_04_045901_create_contact_types_table.php

--- database/migrations/2022_07_04_045901_create_contact_types_table.php
+++ database/migrations/2022_07_04_045901_create_contact_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('contact_types', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unsigned();
             $table->string('description');
             $table->boolean('is_active');
             $table->timestamps();

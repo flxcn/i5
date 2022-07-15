312e672a6dfe2c1847adc28fb7f58660d8b76beb
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-15 00:40:25 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,128 +4,98 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            {{-- <div class="card mb-3">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body text-success">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div> --}}
 
             <div class="card mb-3">
                 <div class="row">
-                <div class="col-5 m-1">
-                    <div class="card-body">
-                        <h5 class="card-title">My Profile</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">firstlast@college.harvard.edu</h6>
-                        <p class="card-text">
-                            Class of 2025<br>
-                            SCAS Member since 2021<br>
-                            User ID: 2534
-                        </p>
-                        <a href="#" class="card-link">Edit profile</a>
-                        <a href="#" class="card-link">Change password</a>
+                    <div class="col-5 m-1">
+                        <div class="card-body">
+                            <h5 class="card-title mb-1">My Profile</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{$user->email}}</h6>
+                            <p class="card-text">
+                                Class of {{$user->grad_year}}<br>
+                                Volunteer since {{$user->created_at->format('Y')}}<br>
+                                User ID: {{$user->id}}
+                            </p>
+                            <a href="#" class="card-link">Edit profile</a>
+                            <a href="#" class="card-link">Change password</a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 m-1">
+                        <div class="card-body">
+                            <h5 class="card-title mb-1">My Contributions</h5>
+                            <ul class="list-group mt-0 list-group-flush text-left">
+                                <li class="list-group-item px-0">Clients entered: <b>{{$user->clients->count();}}</b> (<a href="/search?q={{$user->id}}&mode=author_id">See all</a>)</li>
+                                <li class="list-group-item px-0">Contacts recorded: <b>{{$user->contacts->count();}}</b> (<a href="/clients/author_id={{$user->id}}">See all</a>)</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-
-                <div class="col-6 m-1">
-                    <div class="card-body">
-                        <h5 class="card-title">My Contributions</h5>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Clients entered: <b>9</b> (<a href="/clients/author_id={{Auth::user()->id}}">See all</a>)</li>
-                            <li class="list-group-item">Contacts recorded: <b>12</b> (<a href="/clients/author_id={{Auth::user()->id}}">See all</a>)</li>
-                            {{-- <li class="list-group-item">Contacts edited: <b>3</b></li> --}}
-                        </ul>
-                    </div>
-                </div>
-
-                </div>
-
-                {{-- <div class="card-header">My Recent Contributions</div>
-                    my previous contacts
-
-                    resources
-
-                    contacts/index
-                </div> --}}
             </div>
 
             <div class="card mb-3">
-                <div class="card-header">{{ __('My clients') }}</div>
+                <div class="card-header">{{ __('My recent clients') }}</div>
 
                 <div class="card-body p-4">
+
+                    @unless(count($user->clients) == 0)
+
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
                                 <tr>
-                                <th scope="col">Client ID</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
+                                <th scope="col">Name</th>
                                 <th scope="col">Phone #</th>
                                 <th scope="col">Email</th>
-                                <th scope="col">Language</th>
-                                <th scope="col">State</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @foreach($clients as $client)
+                                @foreach($user->clients->take(5) as $client)
                                     <tr>
-                                        <th scope="row">{{$client->id}}</th>
-                                        <td>{{$client->first_name}}</td>
-                                        <td>{{$client->last_name}}</td>
+                                        <td>{{$client->first_name}} {{$client->last_name}}</td>
                                         <td>{{$client->phone_number}}</td>
                                         <td>{{$client->email}}</td>
-                                        <td>{{$client->language}}</td>
-                                        <td>{{$client->state}}</td>
                                     </tr>
-                                @endforeach  --}}
+                                @endforeach 
                             </tbody>
                         </table>
                     </div>
-                {{-- {{$clients->links()}} --}}
+                    @else
+                    <p>No clients yet.</p>
+                    @endunless
                 </div>
-                
             </div>
 
             <div class="card mb-3">
-                <div class="card-header">{{ __('Recent contacts') }}</div>
+                <div class="card-header">{{ __('My recent contacts') }}</div>
 
                 <div class="card-body p-4">
+                    @unless(count($user->contacts) == 0)
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
                                 <tr>
-                                <th scope="col">Client ID</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Phone #</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Language</th>
-                                <th scope="col">State</th>
+                                    <th scope="col">Contact Type</th>
+                                    <th scope="col">Contact Date</th>
+                                    <th scope="col">Summary</th>
+                                    <th scope="col">Updated At</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @foreach($clients as $client)
+                                @foreach($user->contacts->take(5) as $contact)
                                     <tr>
-                                        <th scope="row">{{$client->id}}</th>
-                                        <td>{{$client->first_name}}</td>
-                                        <td>{{$client->last_name}}</td>
-                                        <td>{{$client->phone_number}}</td>
-                                        <td>{{$client->email}}</td>
-                                        <td>{{$client->language}}</td>
-                                        <td>{{$client->state}}</td>
+                                        <td>{{$contact->contact_type_id}}</td>
+                                        <td>{{$contact->contact_date}}</td>
+                                        <td>{{Str::limit($contact->contact_summary, 200) }}</td>
+                                        <td>{{$client->updated_at}}</td>
                                     </tr>
-                                @endforeach  --}}
+                                @endforeach 
                             </tbody>
                         </table>
                     </div>
-                {{-- {{$clients->links()}} --}}
+                    @else
+                    <p>No contacts yet.</p>
+                    @endunless
                 </div>
                 
             </div>

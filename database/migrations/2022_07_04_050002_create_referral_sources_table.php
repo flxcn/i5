2aee38d61d9b982b6dbc605d5b08d42df17e6920
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-14 22:57:38 -0400

    Update 2022_07_04_050002_create_referral_sources_table.php

--- database/migrations/2022_07_04_050002_create_referral_sources_table.php
+++ database/migrations/2022_07_04_050002_create_referral_sources_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('referral_sources', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unsigned();
             $table->string('description');
             $table->timestamps();
         });

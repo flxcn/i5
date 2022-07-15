c1b82034b192b4a5201c4fd758a30c2303ec72c6
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-15 15:05:57 -0400

    Create search.blade.php

---
+++ resources/views/contacts/search.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            @unless(count($contacts) == 0)
+            
+            <div class="card mb-3">
+                <div class="card-body p-4">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Client Name</th>
+                        <th scope="col">Contact Type</th>
+                        <th scope="col">Contact Date</th>
+                        <th scope="col">Summary</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($contacts as $contact)
+                        <tr>
+                            <th scope="row"><a href="/clients/{{$contact->client->id}}">{{$contact->client->first_name}} {{$contact->client->last_name}}</a></th>
+                            <td>{{$contact->contact_type_id}}</td>
+                            <td>{{$contact->contact_date}}</td>
+                            <td>{{Str::limit($contact->contact_summary, 1000) }}</td>
+                            <td>{{$contact->created_at}}</td>
+                            <td>{{$contact->updated_at}}</td>
+                        </tr>
+                    @endforeach 
+                </tbody>
+            </table>
+            <div>
+                {{ $contacts->withQueryString()->links() }}
+            </div>
+        </div>
+        
+            
+            </div>
+            @else
+                <p>No clients found matching your search. <a href="/clients/create">Click here</a> to add a new client.</p>
+            @endunless
+        </div>
+    </div>
+</div>    
+@endsection

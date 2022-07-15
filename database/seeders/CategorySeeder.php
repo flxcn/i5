1a335c10bd43b01112726fe136bc14b6854de204
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-14 22:55:28 -0400

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,8 +16,6 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        Category::truncate();
-
         $categories =   [
             [
                 'id' => 1,

90a59d4af711f57919a7222dea724f81cb86b469
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-18 00:33:18 -0400

    Update show.blade.php

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -10,12 +10,10 @@
                 <div class="card-body p-4">
 
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center mb-4">
-                        <h2 class="card-title">Contact Info</h2>
+                        <h2 class="card-title">Contact with {{$contact->client->first_name . " " . $contact->client->last_name}} <span class="text-secondary text-sm">#{{$contact->id}}</span></h2>
                         <div class="btn-toolbar pb-3">
                             <div class="btn-group">
-                                <form method="post" 
-                                {{-- action="{{route('contacts.destroy',$contact->id)}}" --}}
-                                >
+                                <form method="post" action="{{route('clients.contacts.destroy', ['client' => $contact->client->id, 'contact' => $contact->id])}}">
                                     @method('delete')
                                     @csrf
                                     <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
@@ -24,32 +22,22 @@
                         </div>
                     </div>
 
-                    <form method="POST" action="/clients/{{$contact->client->id}}/contacts/{{$contact->id}}" enctype="multipart/form-data">
+                    <form method="POST" action="{{route('clients.contacts.update', ['client' => $contact->client->id, 'contact' => $contact->id])}}" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
 
-                        <div class="row mb-3">
-                            <div class="col-2">
-                                <label for="contact_id" class="form-label">ID</label>
-                                <input class="form-control" type="number" name="contact_id" value="{{$contact->id}}" aria-label="Client ID" readonly>
-                            </div>
-                            <div class="col">
+                        <div class="row">
+                            <div class="col-md-3 mb-3">
                                 <label for="contact_date" class="form-label">Contact Date</label>
-                                <input type="text" class="form-control" id="contact_date" name="contact_date" value="{{$contact->contact_date}}">
+                                <input type="date" class="form-control" id="contact_date" name="contact_date" value="{{$contact->contact_date->format('Y-m-d')}}" style="height:2.4rem">
                                 @error('contact_date')
                                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror
                             </div>
-                            <div class="col">
-                                <label for="updated_at" class="form-label">Last updated at</label>
-                                <input type="text" class="form-control" id="updated_at" name="updated_at" value="{{$contact->updated_at}}" readonly>                       
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-4 mb-3">
+                            <div class="col-md-5 mb-3">
                                 <label for="contact_type_id" class="form-label">Referral Source</label>
-                                <select id="contact_type_id" name="contact_type_id" class="form-select">
+                                <select id="contact_type_id" name="contact_type_id" class="form-select" style="height:2.4rem">
                                     @foreach ($contact_types as $contact_type)
                                     <option value={{$contact_type->id}} @selected($contact->contact_type->description == $contact_type->description)>{{$contact_type->description}}</option>
                                     @endforeach
@@ -57,6 +45,11 @@
                                 @error('contact_type_id')
                                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror 
+                            </div>
+
+                            <div class="col-md-4 mb-3">
+                                <label for="updated_at" class="form-label">Last updated at</label>
+                                <input type="datetime-local" class="form-control" id="updated_at" name="updated_at" value="{{$contact->updated_at}}" readonly style="height:2.4rem">                       
                             </div>
                         </div>
 
@@ -70,10 +63,7 @@
                         <hr class="my-3">
                     
                         <div class="mb-1 mt-4">
-                            <button class="btn btn-success" type="submit">
-                            Edit Contact
-                            </button>
-                  
+                            <button class="btn btn-success" type="submit">Edit Contact</button>
                             <a href="/clients/{{$contact->client->id}}" class="btn btn-link"> Back to client </a>
                         </div>
                     </form>

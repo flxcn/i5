f8ef582203f7b20c2e95efa8b15e4073cfd82215
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-18 01:43:28 -0400

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-header">{{ __('SCAS i5 Client Contact Database') }}</div>
 
                 <div class="card-body">
-                    <p>Welcome to the Small Claims Advisory Service's <b>i5 database</b>!<br>You can find the code repository for this platform <a href="https://github.com/flxcn/i5">here</a>.</p> 
+                    <p>Welcome to the Small Claims Advisory Service's <b>i5 database</b>!<br>You can find the code repository for this platform <a href="{{ __('messages.repository_url')}}" target="_blank">here</a>.</p> 
                 </div>
             </div>
 
@@ -18,12 +18,13 @@
                 <div class="card-header text-success">Updates from the i4</div>
 
                 <div class="card-body">
-                    <p>The i5 is a completely rewritten-from-scratch database platform for tracking our client contacts.<br>A few important differences:</p>
+                    <p>The i5 is a database platform completely rewritten from scratch for tracking our client contacts.<br>A few important differences:</p>
                     <ul>
                         <li>Your username is now your @college email.</li>
                         <li>For previous i4 users, please click "Forgot Your Password?" in order to set a new one.</li>
+                        <li>For new SCAS volunteers, please request an invite using your @college email.</li>
                     </ul>
-                    <p>For any questions, please contact <a href="mailto:{{ __('messages.webmaster_email')}}">{{ __('messages.webmaster_name')}}</a>.</p>
+                    <p>If you have any questions, please contact <a href="mailto:{{ __('messages.webmaster_email')}}">{{ __('messages.webmaster_name')}}</a>.</p>
                 </div>
             </div>
         </div>

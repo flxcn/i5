0c2e9b83f7f7d20a291921ef9f9e262e8f263d12
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-18 01:41:28 -0400

    Update messages.php

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,4 +3,7 @@
 return [
     'webmaster_email' => 'felixchen@college.harvard.edu',
     'webmaster_name' => 'Felix Chen',
+    'wiki_url' => 'https://wiki.masmallclaims.org',
+    'office_guide_url' => 'https://docs.google.com/document/d/1iQ0R9BD68KVaa9efyE0KH5nz5BGIMNB5skvZLvMl3CI/pub?embedded=true',
+    'signout_sheet_url' => 'https://docs.google.com/document/d/12ER0ZAqxkdI_AVhl-ZZIRfChu2oBGbRkCvR5HgrkRHE/edit'
 ];

3f8a1b7ae9334599c9e023881b536edf444e14e9
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-18 01:32:04 -0400

    Update search.blade.php

--- resources/views/clients/search.blade.php
+++ resources/views/clients/search.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title', config('app.name', 'i5') . ' | Client Search')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">

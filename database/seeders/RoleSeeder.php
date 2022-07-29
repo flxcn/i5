3c72b6fbeb11a4ee506008b2f9d45e0fdd0b523c
Felix <30987125+flxcn@users.noreply.github.com>
2022-07-29 13:40:16 -0400

    Create RoleSeeder.php

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::doesntHave('roles')->get();
+        foreach($users as $user) {
+            $user->assignRole('Comper');
+        }
+    }
+}

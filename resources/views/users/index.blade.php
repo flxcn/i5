7cbbd8f5f3bc4c4363723acc3be873b59091e1ee
Felix <30987125+flxcn@users.noreply.github.com>
2022-10-19 10:37:41 -0400

    Create edit.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,31 +5,6 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-
-            <div class="d-flex ms-5">  
-                <form action="/users/search" method="get">  
-                    <div class="input-group ms-1">
-                        <a href="/clients/create" class="btn btn-outline-success rounded">Add&nbsp;Client</a>
-            
-                        <input type="text" name="q" class="form-control bg-white ms-2 rounded-start" placeholder="Search the i5..." aria-label="Search the i5..." value= @if(isset($clients)) {{ request()->query('q'); }} @else {{ "" }} @endif>
-                        <button class="btn btn-outline-primary" type="submit">Find&nbsp;Client</button>
-                        <button type="button" class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><button class="dropdown-item" type="submit" name="mode" value="name">By Name</button></li>
-                            <li><button class="dropdown-item" type="submit" name="mode" value="email">By Email</button></li>
-                            <li><button class="dropdown-item" type="submit" name="mode" value="phone">By Phone</button></li>
-            
-                            <li><hr class="dropdown-divider"></li>
-            
-                            <li><a class="dropdown-item" href="/clients">Most Recent</a></li>
-                            <li><button class="dropdown-item" type="submit" name="mode" value="urgent">Most Urgent</button></li>
-                        </ul>
-                    </div>
-                </form>
-            </div>            
 
             @unless(count($users) == 0)
             
